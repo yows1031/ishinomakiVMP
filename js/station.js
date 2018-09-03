f02b4292fc386b57667cbebf3f60e264a61066b7
菅原 洋介 <yow@sugawara-no-MacBook-Pro.local>
2018-09-03 14:42:46 +0900

    no message

--- js/station.js
+++ js/station.js
@@ -2,6 +2,15 @@
     'use strict';
     var currentNum = 0;
 
+    var mask = document.getElementById('mask');
+    var back = document.getElementById('back');
+
+    // Menu's icons
+
+    var btn_station = document.getElementById('btn_station');
+    var btn_cityHall = document.getElementById('btn_cityHall');
+
+    // 市役所
     var cityHallArray = [
         'img/pic/station/cityhall/cityhall1.jpg',
         'img/pic/station/cityhall/cityhall2.jpg',
@@ -12,60 +21,46 @@
     var cityHallprev = document.getElementById('cityHallprev');
     var cityHallnext = document.getElementById('cityHallnext');
     var cityHall_target = document.getElementById('cityHall_target');
-    var cityhall_thumbnails = document.getElementById('cityhall_thumbnails');
-
-//     var cityHallprev　= document.getElementById('cityHallprev');
-//     var cityHallnext = document.getElementById('cityHallnext');
-//     var cityHall_target = document.getElementById('cityHall_target');
-//     var cityHallArray_thumbnails = document.getElementById('cityhallArray_thumbnails');
-//     var mask = document.getElementById('mask');
-// //    var cityHall = document.getElementById('cityHall');
-//     var bt_cityhall = document.getElementById('bt_cityhall');
-//     var back = document.getElementById('back');
+    var cityHall_thumbnails = document.getElementById('cityhall_thumbnails');
 
 
-function create_cityHall_thumbnails(){
-    var i;
-    var li;
-    var img;
-    for(i = 0; i < cityHallArray.length; i++){
-        li = document.createElement('li');
-        li.addEventListener('click', function(){
-            cityHall_target.src = this.children[0].src;
-        });
-        img = document.createElement('img');
-        img.src = cityHallArray[i];
-        li.appendChild(img);
-        cityhall_thumbnails.appendChild(li);
+    function create_cityHall_thumbnails(){
+        var i;
+        var li;
+        var img;
+        for(i = 0; i < cityHallArray.length; i++){
+            li = document.createElement('li');
+            li.addEventListener('click', function(){
+                cityHall_target.src = this.children[0].src;
+            });
+            img = document.createElement('img');
+            img.src = cityHallArray[i];
+            li.appendChild(img);
+            cityHall_thumbnails.appendChild(li);
+        }
     }
-}
 
-create_cityHall_thumbnails();
-cityhall_thumbnails.children[currentNum].className = 'current';
+    create_cityHall_thumbnails();
+    cityHall_thumbnails.children[currentNum].className = 'current';
 
-cityHallprev.addEventListener('click', function(){
-    cityhall_thumbnails.children[currentNum].className = '';
-    currentNum--;
-    if(currentNum < 0){
-        currentNum = 4;
-    }
-    cityHall_target.src = cityHallArray[currentNum];
-    cityhall_thumbnails.children[currentNum].className = 'current';
-});
+    cityHallprev.addEventListener('click', function(){
+        cityHall_thumbnails.children[currentNum].className = '';
+        currentNum--;
+        if(currentNum < 0){
+            currentNum = 4;
+        }
+        cityHall_target.src = cityHallArray[currentNum];
+        cityHall_thumbnails.children[currentNum].className = 'current';
+    });
 
-cityHallnext.addEventListener('click', function(){
-    cityhall_thumbnails.children[currentNum].className = '';
-    currentNum++;
-    if(currentNum > 4){
-        currentNum = 0;
-    }
-    cityHall_target.src = cityHallArray[currentNum];
-    cityhall_thumbnails.children[currentNum].className = 'current';        
-});
-    
-    // bt_cityhall.addEventListener('click', function(){
-    //     mask.className = "";
-    //     cityhall.className = "";
-    // });
+    cityHallnext.addEventListener('click', function(){
+        cityHall_thumbnails.children[currentNum].className = '';
+        currentNum++;
+        if(currentNum > 4){
+            currentNum = 0;
+        }
+        cityHall_target.src = cityHallArray[currentNum];
+        cityHall_thumbnails.children[currentNum].className = 'current';        
+    });
 
 })();
